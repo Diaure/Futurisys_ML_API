f489a18cbf21c3cd12fa0b3ceb8ed048f0052d6c
Aurélie GABU <aurelie.gabu@gmail.com>
2026-01-15 23:37:15 +0100

    removing db connexion on HF

--- scripts/create_tables.py
+++ scripts/create_tables.py
@@ -1,6 +1,9 @@
-from App.database import engine
-from App.database import Base
+from App.database import engine, Base
+
 
 Base.metadata.create_all(bind=engine)
-
-print("Tables créées avec succès")
+if engine is None: 
+    print("DB désactivée : aucune table créée.") 
+else: 
+    Base.metadata.create_all(bind=engine) 
+    print("Tables créées avec succès")
