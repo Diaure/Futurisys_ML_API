3553af2678aee59941f30fbaec5d3ce533fcccbe
Aurélie GABU <aurelie.gabu@gmail.com>
2026-01-15 23:37:15 +0100

    removing db connexion on HF

--- App/database.py
+++ App/database.py
@@ -3,13 +3,20 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# Tentative d'import SQLAlchemy uniquement si disponible 
+try: 
+    SQLALCHEMY_AVAILABLE = True 
+except ModuleNotFoundError: 
+    SQLALCHEMY_AVAILABLE = False
+
 load_dotenv()
 
 # Détection si on est en CI (GitHub Actions) ou en test 
 IS_CI = os.getenv("CI") == "true"
 IS_PYTEST = "pytest" in os.getenv("PYTHONPATH", "") or os.getenv("PYTEST_CURRENT_TEST") is not None 
+IS_HF = os.getenv("SPACE_ID") is not None # Hugging Face 
 
-SKIP_DB = IS_CI or IS_PYTEST
+SKIP_DB = IS_CI or IS_PYTEST or IS_HF or not SQLALCHEMY_AVAILABLE
 
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
@@ -19,7 +26,7 @@ DB_NAME = os.getenv("DB_NAME", "test_db")
 
 DATABASE_URL = (f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}"f"@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 
-Base = declarative_base()
+Base = declarative_base() if SQLALCHEMY_AVAILABLE else None
 
 if not SKIP_DB:
     engine = create_engine(DATABASE_URL)
